50c5902e2d06643b5a28ad634d967b22ba9d6648
Michael McCartney <michael@mccartn3y.com>
2021-05-25 14:58:19 +0200

    Extracted info about appt and added to whatsapp

--- sirfy_impfung.py
+++ sirfy_impfung.py
@@ -23,16 +23,40 @@ br.set_handle_robots(False)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 params = {"versichert": "", "terminsuche": "", "uniqueident": "607feb7a343fb"}
 data = urllib.parse.urlencode(params)
-
+notified_id = []
+notify_wait = 0
 while True:
-    resp = br.open("https://onlinetermine.zollsoft.de/includes/searchTermine_app_feature.php", data)
-    resp_dict = json.loads(BeautifulSoup(resp, 'html.parser').prettify())
-    if len(resp_dict['terminsuchen']) >0 or len(resp_dict['termine']) > 0:
-        print(resp_dict)
-        message = client.messages.create( 
-                              from_='whatsapp:+14155238886',  
-                              body='Your appointment is coming up on July 21 at 3PM',      
-                              to=num 
-                          ) 
-    time.sleep(10)
+    try:
+        resp = br.open("https://onlinetermine.zollsoft.de/includes/searchTermine_app_feature.php", data)
+        resp_dict = json.loads(BeautifulSoup(resp, 'html.parser').prettify())
+        if len(resp_dict['termine']) > 0:
+            appt_dict = []
+            for termine in resp_dict['termine']:
+                temp_dict = {}
+                temp_dict['Date'] = termine[0]
+                temp_dict['time'] = termine[1]
+                temp_dict['id'] = termine[2]
+                temp_dict['doc'] = termine[3]
+                temp_dict['desc'] = termine[4]
+                appt_dict.append(temp_dict)
+                print("{}: Appointment for {} on {} at {} with {}".format(datetime.now().strftime("%d/%m/%y %H:%M:%S"),
+                                                                      temp_dict['desc'],
+                                                                      temp_dict['Date'],
+                                                                      temp_dict['time'],
+                                                                      temp_dict['doc']))
+            # send message if one hasn't been sent about this appointment and in the last 30 secs
+            for termine in appt_dict:
+                if notify_wait > 6 and termine['id'] not in notified_id:
+                    message = client.messages.create( 
+                                          from_='whatsapp:+14155238886',  
+                                          body='Your appointment is coming up on {}  at {}'.format(temp_dict['Date'],
+                                                                      temp_dict['time']),      
+                                          to=num 
+                                      ) 
+                    notified_id.append(termine['id'])
+                    notify_wait = 0
+        notify_wait +=1
+    except:
+        pass
+    time.sleep(5)
     
