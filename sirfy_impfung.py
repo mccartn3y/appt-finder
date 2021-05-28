e98bb3d6eb93934cc2ec31b87c457eb6aa494838
mccartn3y <30482268+mccartn3y@users.noreply.github.com>
2021-05-28 08:41:36 +0200

    added datetime import

--- sirfy_impfung.py
+++ sirfy_impfung.py
@@ -6,7 +6,7 @@ import time
 import mechanize
 import ssl
 from twilio.rest import Client
-from getpass import getpass
+import datetime
 from bs4 import BeautifulSoup
 
 ssl._create_default_https_context = ssl._create_unverified_context
